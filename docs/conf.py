5d66636a768c26d9929f2a63307c239c702932c0
Alex Millane <amillane@nvidia.com>
2026-01-28 10:23:19 +1300

    lint.

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinx_tabs.tabs',
     'sphinx_copybutton',
-    "sphinx_multiversion",
+    'sphinx_multiversion',
     # TODO(alexmillane, 2025-04-24): Try re-enabling this once we have pydocs generating.
     #    'autodocsumm'
     'nvblox_torch_doc_tools'
@@ -117,10 +117,10 @@ html_static_path = ['_static']
 html_css_files = ['custom.css']
 
 # Versioning (sphinx-multiversion)
-smv_remote_whitelist = r"^.*$"
-smv_branch_whitelist = r"^(public|v0.0.8|v0.0.9)$"
-smv_tag_whitelist = r"^(v0.0.8|v0.0.9)$"
-html_sidebars = {"**": ["versioning.html", "sidebar-nav-bs"]}
+smv_remote_whitelist = r'^.*$'
+smv_branch_whitelist = r'^(public|v0.0.8|v0.0.9)$'
+smv_tag_whitelist = r'^(v0.0.8|v0.0.9)$'
+html_sidebars = {'**': ['versioning.html', 'sidebar-nav-bs']}
 
 # Todos
 todo_include_todos = True
